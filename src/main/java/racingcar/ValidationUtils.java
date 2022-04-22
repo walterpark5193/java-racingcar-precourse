ee88e736dc840f204f974e0a202f5532d7529c8c
walterpark5193 <walterpark5193@gmail.com>
2022-04-22 18:38:00 +0900

    feat(validationUtils) : 입력 값 체크 (공백여부/이름 길이 5자이내/쉼표만 입력된 경우) 구현 및 테스트 작성

--- src/main/java/racingcar/ValidationUtils.java
+++ src/main/java/racingcar/ValidationUtils.java
@@ -4,15 +4,48 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class ValidationUtils {
-    public static boolean validName(String name) {
+public class ValidationUtils extends IllegalArgumentException{
+    public boolean validName(String userInput) {
         List<String> nameList = new ArrayList<>();
 
         //입력받은 이름 리스트 분리
-        Collections.addAll(nameList, name.split(","));
+        splitNameList(userInput, nameList);
+        System.out.println("userInput = "+userInput);
+        try{
+            //이름 입력 값 체크 (아무것도 입력되지 않았거나 쉼표만 입력된 경우)
+            validNameValue(nameList);
 
-        //이름 입력 값 체크 (아무것도 입력되지 않았거나 쉼표만 입력된 경우)
-        return nameList.size() != 0 && nameList.stream().noneMatch(s -> s.length() == 0 || s.length() > 5);
-        //이름 길이 체크 (이름값이 입력되지 않았거나 5자리 초과한 경우 에러)
+            //이름 길이 체크 (이름값이 입력되지 않았거나 5자리 초과한 경우 에러)
+            validNameLength(nameList);
+        } catch(IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getClass().getName());
+            //throw new IllegalArgumentException();   //에러 대신 다시 입력받도록 해야함
+            return false;
+        }
+        return true;
+    }
+
+    private boolean splitNameList(String name, List<String> nameList) {
+        return Collections.addAll(nameList, name.split(","));
+    }
+
+    private void validNameValue(List<String> nameList){
+        if(nameList.size() == 0){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validNameLength(List<String> nameList) {
+        if (nameList.stream().anyMatch(s -> s.length() == 0 || s.length() > 5))
+            throw new IllegalArgumentException();
+    }
+
+    public static boolean validRepeat(String repeatCnt) throws IllegalArgumentException {
+        try {
+            return (!repeatCnt.isEmpty() && !(Integer.parseInt(repeatCnt) < 1));
+        } catch(IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getClass().getName());
+            return false;
+        }
     }
 }
--- src/test/java/racingcar/ValidationUtilsTest.java
+++ src/test/java/racingcar/ValidationUtilsTest.java
@@ -3,19 +3,49 @@ package racingcar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ValidationUtilsTest {
 
     @Test
     @DisplayName("자동차_입력값_이름_검증")
-    void 자동차_입력값_이름_검증(){
-        assertThat(ValidationUtils.validName("pobi")).isTrue();
-        assertThat(ValidationUtils.validName("pobi,java")).isTrue();
-        assertThat(ValidationUtils.validName("pobi,javaji")).isFalse();
-        assertThat(ValidationUtils.validName("")).isFalse();
-        assertThat(ValidationUtils.validName(",")).isFalse();
-        assertThat(ValidationUtils.validName(",,,")).isFalse();
+    void car_input_validation(){
+        assertThat(new ValidationUtils().validName("pobi")).isTrue();
+        assertThat(new ValidationUtils().validName("pobi,java")).isTrue();
+        assertThat(new ValidationUtils().validName("pobi,javaji")).isFalse();
+        assertThat(new ValidationUtils().validName("")).isFalse();
+        assertThat(new ValidationUtils().validName(",")).isFalse();
+        assertThat(new ValidationUtils().validName(",,,")).isFalse();
+    }
+
+    @Test
+    @DisplayName("게임_경주_횟수_검증")
+    void repeatCount_validation(){
+        assertThat(ValidationUtils.validRepeat("1")).isTrue();
+        assertThat(ValidationUtils.validRepeat("9")).isTrue();
+        assertThat(ValidationUtils.validRepeat("10")).isTrue();
+        assertThat(ValidationUtils.validRepeat("0")).isFalse();
+        assertThat(ValidationUtils.validRepeat("-1")).isFalse();
+        assertThat(ValidationUtils.validRepeat("-")).isFalse();
+        assertThat(ValidationUtils.validRepeat("abc")).isFalse();
+
+        /*
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> {
+                    ValidationUtils.validRepeat("-");
+                }).isInstanceOf(IllegalArgumentException.class)
+        );
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> {
+                    ValidationUtils.validRepeat("abc");
+                }).isInstanceOf(IllegalArgumentException.class)
+        );
+
+         */
+
+
     }
 
 }
