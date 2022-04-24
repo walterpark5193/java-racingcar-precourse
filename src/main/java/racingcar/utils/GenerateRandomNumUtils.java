b79b2a6dfa2c99bf2169bac4602336b2facfcec1
walterpark5193 <walterpark5193@gmail.com>
2022-04-25 03:05:33 +0900

    Refactor(GenerateRandomNumUtils) : 상수 enum 구현으로 인한 리팩토링

--- src/main/java/racingcar/utils/GenerateRandomNumUtils.java
+++ src/main/java/racingcar/utils/GenerateRandomNumUtils.java
@@ -3,10 +3,8 @@ package racingcar.utils;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class GenerateRandomNumUtils {
-    public static final int MIN_VALUE = 0;
-    public static final int MAX_VALUE = 9;
 
     public int generateRandomNum(){
-        return Randoms.pickNumberInRange(MIN_VALUE, MAX_VALUE);
+        return Randoms.pickNumberInRange(ValidNumberConditionEnum.MIN_VALUE.getValue(), ValidNumberConditionEnum.MAX_VALUE.getValue());
     }
 }
