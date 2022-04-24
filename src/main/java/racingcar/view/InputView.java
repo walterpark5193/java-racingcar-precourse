32c944eec8fb8696456718d5e86a1e7466687bbf
walterpark5193 <walterpark5193@gmail.com>
2022-04-24 20:25:00 +0900

    Feat(InputView) : 사용자 입력을 위한 메시지 출력 및 입력 받는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,13 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    public static final String INPUT_CAR_NAME_MSG = "경주할 자동차 이름 (이름은쉼표(,) 기준으로 구분)";
+    public static final String INPUT_REPEAT_CNT_MSG = "시도할 횟수";
+
     public static String getCarsNameInput() {
-        System.out.println("경주할 자동차 이름 (이름은쉼표(,) 기준으로 구분)");
+        System.out.println(INPUT_CAR_NAME_MSG);
         return Console.readLine();
     }
 
-    public static String getPlayRepeatnput() {
-        System.out.println("시도할 횟수");
+    public static String getPlayRepeatInput() {
+        System.out.println(INPUT_REPEAT_CNT_MSG);
         return Console.readLine();
     }
 }
