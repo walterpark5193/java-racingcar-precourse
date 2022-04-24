ef58442c4e5c484094ca77ca13c3a0e5dc3832d7
walterpark5193 <walterpark5193@gmail.com>
2022-04-25 03:17:58 +0900

    Refactor : 상수 enum 구현으로 인한 리팩토링

--- src/main/java/racingcar/utils/ValidCommonConditionEnum.java
+++ src/main/java/racingcar/utils/ValidCommonConditionEnum.java
@@ -1,14 +1,14 @@
 package racingcar.utils;
 
-import java.io.Serializable;
-
 public enum ValidCommonConditionEnum {
     DELIMITER(","),
     REGEX_NUM("[0-9]+"),
     RESULT_MSG("실행결과"),
     RESULT_PREFIX(" : "),
     SCORE_VALUE("-"),
-    WINNER_MSG("최종 우승자 : ");
+    WINNER_MSG("최종 우승자 : "),
+    INPUT_CAR_NAME_MSG("경주할 자동차 이름 (이름은쉼표(,) 기준으로 구분)"),
+    INPUT_REPEAT_CNT_MSG("시도할 횟수");
 
     private final String value;
 
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,19 +1,17 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.utils.ValidCommonConditionEnum;
 
 public class InputView {
 
-    public static final String INPUT_CAR_NAME_MSG = "경주할 자동차 이름 (이름은쉼표(,) 기준으로 구분)";
-    public static final String INPUT_REPEAT_CNT_MSG = "시도할 횟수";
-
     public static String getCarsNameInput() {
-        System.out.println(INPUT_CAR_NAME_MSG);
+        System.out.println(ValidCommonConditionEnum.INPUT_CAR_NAME_MSG.getValue());
         return Console.readLine();
     }
 
     public static String getPlayRepeatInput() {
-        System.out.println(INPUT_REPEAT_CNT_MSG);
+        System.out.println(ValidCommonConditionEnum.INPUT_REPEAT_CNT_MSG.getValue());
         return Console.readLine();
     }
 }
