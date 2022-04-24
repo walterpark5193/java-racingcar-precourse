6edf37607303db6310278ea1cb68cc1919df21c8
walterpark5193 <walterpark5193@gmail.com>
2022-04-25 01:13:43 +0900

    Feat(ManageCar) : 우승자 판단 및 출력 로직 구현, Stream 제거

--- src/main/java/racingcar/domain/ManageCar.java
+++ src/main/java/racingcar/domain/ManageCar.java
@@ -4,23 +4,32 @@ import racingcar.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 
 public class ManageCar {
-    private List<CarInfo> carInfoList = new ArrayList<>();
+    public static final String DELIMITER = ",";
+    private List<CarInfo> carInfoList;
     private int playRepeatCnt;
+    private int highScore;
+    private List<String> winnerList;
 
 
     public ManageCar(String userInputCarNames, String repeatCnt) {
+        clearGameInfo();
         createCar(userInputCarNames);
         this.playRepeatCnt = Integer.parseInt(repeatCnt);
+    }
+
+    private void clearGameInfo() {
+        carInfoList = new ArrayList<>();
+        winnerList = new ArrayList<>();
+        highScore = Integer.MIN_VALUE;
     }
 
     private void createCar(String userInputCarNames) {
         //1. 자동차 입력받는 기능 구현 완료
         //Validation 완료 후 실제 자동차 객체 생성
-        for (String name : userInputCarNames.split(",")) {
+        for (String name : userInputCarNames.split(DELIMITER)) {
             this.carInfoList.add(new CarInfo(name));
         }
     }
@@ -30,23 +39,41 @@ public class ManageCar {
     }
 
     public void playGame() {
-        System.out.println("playRepeatCnt = " + playRepeatCnt);
-        IntStream.range(0, playRepeatCnt).forEach(
-                this::accept
-        );
-        //6. 우승자 판별 로직(가장 높은 전진cnt)
-        //7. 인덱스 위치의 자동차 이름 반환하는 로직
-        //8. 우승자 출력하는 로직
+        for(int i=0; i<playRepeatCnt; i++) {
+            playGameResult(i);
+        }
 
+        for (CarInfo carInfo : carInfoList) {
+            getWinnerInfo(winnerList, carInfo);
+        }
+
+        //8. 우승자 출력하는 로직
+        OutputView.printWinnerMsg(winnerList);
+    }
+
+    private void getWinnerInfo(List<String> winnerList, CarInfo carInfo) {
+        //6. 가장 많이 전진한 자동차의 스코어 구하는 로직
+        //7. 우승자 List 구하는 로직
+        if (carInfo.getForwardCount() >= highScore) {
+            resetWinnerInfo(winnerList, carInfo.getForwardCount());
+            winnerList.add(carInfo.getName());
+        }
+    }
+
+    private void resetWinnerInfo(List<String> winnerList, int score) {
+        if(score > this.highScore){
+            winnerList.clear();
+        }
+        this.highScore = score;
     }
 
     private void carMoveForward() {
         //0~9사이의 랜덤 숫자 생성하는 로직
         //4이상일 시 전진대상으로 판단하는 로직
         //콜렉션 개체의 전진카운트 증가시키는 로직
-        IntStream.range(0, carInfoList.size()).forEach(
-                i -> carInfoList.get(i).moveForward()
-        );
+        for (CarInfo carInfo : carInfoList) {
+            carInfo.moveForward();
+        }
     }
 
     private void printCurrentResultMsg() {
@@ -55,19 +82,12 @@ public class ManageCar {
 
     private void printCurrentPoint() {
         //5. 현재 누적된 경주 결과 출력하는 로직
-        carInfoList.stream().map(this::getCurrentPoint).forEach(OutputView::printCurrentPoint);
-    }
-
-    private String getCurrentPoint(CarInfo carInfo) {
-        StringBuilder msg = new StringBuilder(carInfo.getName() + " : ");
-        for (int j = 0; j < carInfo.getForwardCount(); j++) {
-            msg.append("-");
+        for (CarInfo carInfo : carInfoList) {
+            OutputView.printCurrentPoint(carInfo);
         }
-        return msg.toString();
     }
 
-
-    private void accept(int i) {
+    private void playGameResult(int i) {
         //3. 전진/멈춤 조건 판별하는 기능
         //4. 전진 횟수 누적하는 기능
         carMoveForward();
