55cd140031672f73d8532ea2aaa04aafe37dd74f
walterpark5193 <walterpark5193@gmail.com>
2022-04-25 01:15:08 +0900

    Feat(Application) : RacingCar 시작 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,12 @@
 package racingcar;
 
+
+import racingcar.controller.RacingCar;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
-        // 1. RacingCar
-        // 2. 시도 횟수 입력 받는 기능
-
+        // 게임 시작
+        new RacingCar().startGame();
     }
 }
