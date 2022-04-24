356b7ec76e7e7ed5be99869375283175fba1572f
walterpark5193 <walterpark5193@gmail.com>
2022-04-24 20:23:17 +0900

    Feat(Car) : 랜덤넘버 생성 및 전진여부 판단 기능 구현

--- src/main/java/racingcar/domain/CarInfo.java
+++ src/main/java/racingcar/domain/CarInfo.java
@@ -1,8 +1,12 @@
 package racingcar.domain;
 
+import racingcar.utils.GenerateRandomNumUtils;
+
 public class CarInfo {
     private String name;
     private int forwardCount;
+
+    public static final int MOVING_FORWARD = 4;
 
     public CarInfo(String carName){
         this.name = carName;
@@ -24,4 +28,21 @@ public class CarInfo {
     public void setForwardCount(int forwardCount) {
         this.forwardCount = forwardCount;
     }
+
+    public void moveForward(){
+        if (isForward()) {
+            this.setForwardCount(this.getForwardCount()+1);
+        }
+    }
+
+    private boolean isForward() {
+        int b = generateRandomForwardNum();
+        System.out.println("gen random num = "+b);
+        return b >= MOVING_FORWARD;
+    }
+
+    private int generateRandomForwardNum() {
+        return new GenerateRandomNumUtils().generateRandomNum();
+    }
+
 }
